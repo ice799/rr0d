6613db1b174fb501cb2318ee30ae6dc5541ab456
joe <joe@joe-desktop.(none)>
2008-12-24 23:00:00 -0800

    adding some basic types to make life easier

---
+++ 0.3/types.h
@@ -0,0 +1,13 @@
+#if !defined(__TYPES_H__)
+#define __TYPES_H__
+typedef unsigned char            u8;   // 8 bit type 
+typedef unsigned short          u16;   // 16 bit type 
+typedef unsigned long           u32;   // 32 bit type 
+typedef unsigned long long      u64;   // 64 bit type 
+
+typedef char                     i8;   // 8 bit signed type 
+typedef short                   i16;   // 16 bit signed type 
+typedef long                    i32;   // 32 bit signed type 
+typedef long long               i64;   // 64 bit signed type 
+
+#endif /* !defined(__TYPES_H__) */
