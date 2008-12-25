7369b173371073717006de17c40890b3f96e9a78
joe <joe@joe-desktop.(none)>
2008-12-24 23:50:43 -0800

    move some function declarations up; create a union

--- 0.3/idt.h
+++ 0.3/idt.h
@@ -6,7 +6,15 @@
 unsigned int* getbaseidt(void);
 unsigned int* getbasegdt(void);  
 unsigned int getbaseldt(void);
+unsigned int translate_logic_to_linear(unsigned int , unsigned int );
+void  visualise_idt(unsigned long *);
+unsigned long modifie_idt(unsigned int ,unsigned int *);
+unsigned long read_idt_entry(unsigned int *);
 
+union descriptor {
+  struct gate_descriptor gd;
+  struct segment_descriptor sd;
+};
 
 struct segment_descriptor {
   unsigned limit0:16;
